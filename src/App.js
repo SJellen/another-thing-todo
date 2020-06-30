68fc801a5c29de20c0b7048de4248b319c3fa9c9
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-06-30 17:59:10 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TododList'
+
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos'
+
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => { 
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+     if (storageTodos) {
+      setTodos(storageTodos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+   
+  }, [todos])
+
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos])
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      }
+    ))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Another Thing ToDo</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete}/>
     </div>
   );
 }
